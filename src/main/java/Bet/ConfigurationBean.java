81f60ed21cfe650ce7b7d3838c92d860fe18c717
Carlos Born <carlosedu.born@gmail.com>
2024-07-16 16:27:00 -0300

    Adicionado novas configurações no bean

--- src/main/java/Bet/ConfigurationBean.java
+++ src/main/java/Bet/ConfigurationBean.java
@@ -1,7 +1,11 @@
 package Bet;
 
+import com.bets.App.Controllers.CrashBetController;
 import com.bets.App.Controllers.CrashRoundController;
+import com.bets.App.Controllers.UserController;
+import com.bets.App.Repositories.CrashBetRepository;
 import com.bets.App.Repositories.CrashRoundRepository;
+import com.bets.App.Repositories.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
@@ -12,6 +16,16 @@ public class ConfigurationBean {
     @Bean
     public CrashRoundController crashRoundController(CrashRoundRepository crashRoundRepository) {
         return new CrashRoundController(crashRoundRepository);
+    }
+
+    @Bean
+    public CrashBetController crashBetController(CrashBetRepository crashBetRepository) {
+        return new CrashBetController(crashBetRepository);
+    }
+
+    @Bean
+    public UserController userController(UserRepository userRepository) {
+        return new UserController(userRepository);
     }
 
 }
